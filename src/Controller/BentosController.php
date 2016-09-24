8d851198c356b943a6b6ca48e3d38f6e40338712
kudotakuya <ttt.ttt.7733@gmail.com>
2016-09-24 15:11:56 +0900

    create model

--- src/Controller/BentosController.php
+++ src/Controller/BentosController.php
@@ -2,12 +2,19 @@
 
 namespace App\Controller;
 
+use Cake\ORM\TableRegistry;
+
 class BentosController extends AppController{
 
     public $name = 'Bentos';
 
     public function index(){
-                
+        $tableBento = TableRegistry::get('Bentos');
+        $query = $tableBento->find();
+        foreach ($query as $row) {
+            debug($row); // $rowの型はCake\ORM\Entity
+            echo $row->name;
+        }
                         
     }
 }
