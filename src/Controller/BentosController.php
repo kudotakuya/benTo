66fb0a77ba9de93caaa2806d5cad9b764eb418f2
kudotakuya <ttt.ttt.7733@gmail.com>
2016-09-25 04:30:53 +0900

    make json

--- src/Controller/BentosController.php
+++ src/Controller/BentosController.php
@@ -150,6 +150,8 @@ $this->response->header('Access-Control-Allow-Origin', '*');
         $data = ('bento_id' => 1, 'menu_id' => 3, 'want_menu_id' => 5);
         $stage = $stagesTable->patchEntity($stage, $data);
         $stagesTable->save($table);
+        $this->set(compact('stage'));
+        $this->set('_serialize', ['stage']);
     }
 }
 ?>
