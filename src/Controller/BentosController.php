823ee586c73470c68a28dc61d3f648eed7d346ba
kudotakuya <ttt.ttt.7733@gmail.com>
2016-09-25 03:25:01 +0900

    make json

--- src/Controller/BentosController.php
+++ src/Controller/BentosController.php
@@ -144,20 +144,16 @@ $this->response->header('Access-Control-Allow-Origin', '*');
 	}
     
     public function addWant(){
-	  $this->response->header('Access-Control-Allow-Origin', '*');
+	  	$this->response->header('Access-Control-Allow-Origin', '*');
  
-        $this->Stages = TableRegistry::get('Stages');
-        
-		$bento_id = $this->request->data('bento_id');
-        $menu_id = $this->request->data('menu_id');
-        $status = $this->request->data('status');
-		$data = [
-        	'bento_id' => 1,
-        	'menu_id' => 3,
-			'want_menu_id' => 5
-		];
-		$entity = $this->Stages->patchEntity($entity, $data); 
-		$this->Stages->save($entity);   
+   		$stagesTable = TableRegistry::get('Stages');
+		$stage = $stagesTable->newEntity();
+
+		$stage->bento_id = 1;
+		$stage->menu_id = 3;
+		$stage->want_menu_id = 5;
+
+		$stagesTable->save($stage);
 	}
 }
 
