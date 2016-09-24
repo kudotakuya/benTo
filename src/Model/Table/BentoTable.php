bad4acb53399cdb162733c64909411e2c9d1a4d6
kudotakuya <ttt.ttt.7733@gmail.com>
2016-09-24 15:58:46 +0900

    make json

--- src/Model/Table/BentoTable.php
+++ src/Model/Table/BentoTable.php
@@ -6,7 +6,7 @@ use Cake\ORM\Table;
 class BookTitleTable extends Table {
 
     public function initialize(array $config) {
-        $this->table('bento_menu');
+        $this->table('bento_menus');
         $this->hasMany('bento_menus',[
             'foreignKey' => 'bento_id',
         ]);
