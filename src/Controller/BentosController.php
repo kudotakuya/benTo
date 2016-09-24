cff112263a722e846b7013c017084273d0f6f0a6
kudotakuya <ttt.ttt.7733@gmail.com>
2016-09-25 03:39:12 +0900

    make json

--- src/Controller/BentosController.php
+++ src/Controller/BentosController.php
@@ -144,16 +144,8 @@ $this->response->header('Access-Control-Allow-Origin', '*');
 	}
     
     public function addWant(){
-	//  	$this->response->header('Access-Control-Allow-Origin', '*');
- 
-   		$stagesTable = TableRegistry::get('Stages');
-		$stage = $stagesTable->newEntity();
-
-		$stage->bento_id = 1;
-		$stage->menu_id = 3;
-		$stage->want_menu_id = 5;
-
-	}
+	  	$this->response->header('Access-Control-Allow-Origin', '*');
+}
 }
 
 ?>
