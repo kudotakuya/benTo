0a7b7b31d6a0bae157a12ff55993ae7859dd6701
kudotakuya <ttt.ttt.7733@gmail.com>
2016-09-24 17:19:54 +0900

    make json

--- src/Model/Table/BentosTable.php
+++ src/Model/Table/BentosTable.php
@@ -8,16 +8,16 @@ class BentosTable extends Table {
 
     public function initialize(array $config) {
 
-        parent::initialize($config);
+//        parent::initialize($config);
 
 //        $this->table('Bentos');
     //    $this->displayField('name');
 //        $this->primaryKey('id');
         
       //  $this->table('bento');
-        $this->hasMany('Bento_menus, [
-            'foreignKey' => 'bento_id',
-            ]);
+//        $this->hasMany('Bento_menus, [
+//            'foreignKey' => 'bento_id',
+//            ]);
     }
 
 }
