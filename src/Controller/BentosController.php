737b98a04f96b99abe320b8f8f95b1455b751447
kudotakuya <ttt.ttt.7733@gmail.com>
2016-09-25 02:13:25 +0900

    make json

--- src/Controller/BentosController.php
+++ src/Controller/BentosController.php
@@ -85,6 +85,12 @@ $this->response->header('Access-Control-Allow-Origin', '*');
             'contain'=>['Stages']
         ]);
 print_r($query->toArray());
+          $bentoArray = array();
+          foreach ($query->toArray() as $row) {
+                array_push($bentoArray, $row['stages']);
+             }
+          }
+	
 //         $query = $this->Bentos->find()
 //   			 ->hydrate(false)
 //   			 ->join([
