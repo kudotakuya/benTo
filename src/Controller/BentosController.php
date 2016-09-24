7533bc18999506f407109b36fb7f4157bd271f9a
kudotakuya <ttt.ttt.7733@gmail.com>
2016-09-24 20:46:55 +0900

    make json

--- src/Controller/BentosController.php
+++ src/Controller/BentosController.php
@@ -38,7 +38,7 @@ class BentosController extends AppController{
         $menu_id = $this->request->data('menu_id');
         $status = $this->request->data('status');
 
-        $this->BentoMenus = TableRegistry::get('bento_menus');
+        $this->BentoMenus = TableRegistry::get('BentoMenus');
         $this->BentoMenus->updateAll(
             array ( 'flag' =>1),
             array ( 'bento_id' => 1, 'menu_id' => 1 ) );
