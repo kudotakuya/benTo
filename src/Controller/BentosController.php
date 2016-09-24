ac0608b3c306fae21eb719650f160280742f616b
kudotakuya <ttt.ttt.7733@gmail.com>
2016-09-24 16:48:27 +0900

    make json

--- src/Controller/BentosController.php
+++ src/Controller/BentosController.php
@@ -9,9 +9,9 @@ class BentosController extends AppController{
     public $name = 'Bentos';
 
     public function index(){
-      //  $this->loadModel('Bentos');
+        $this->loadModel('Bentos');
       //  $tableBento = TableRegistry::get('Bentos');
-        $query = $this->Bentos->find();
+        $query = $this->Bentos->find('all');
         $bentoArray = array();
         foreach ($query as $row) {
             
